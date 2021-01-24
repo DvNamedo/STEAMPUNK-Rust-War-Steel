8aa08dcd0fde839cd8a462231ac7fa61f14f71ba
eukkbi <eukkbi6@gmail.com>
2021-01-24 21:43:04 +0900

    1/24

--- src/JMTemplate/content/ModLoader.java
+++ src/JMTemplate/content/ModLoader.java
@@ -5,10 +5,10 @@ import JMTemplate.content.*;
 
 public class ModLoader implements ContentList {
     private final ContentList[] contents = {new SBlocks()};
+
     public void load() {
         for(ContentList list : contents){
             list.load();
         }
     }
-
-    }
+}
--- src/JMTemplate/content/SBlocks.java
+++ src/JMTemplate/content/SBlocks.java
@@ -3,9 +3,10 @@ package JMTemplate.content;
 import mindustry.content.Items;
 import mindustry.ctype.ContentList;
 import mindustry.type.Category;
-import mindustry.type.ItemStack;
 import mindustry.world.Block;
 import mindustry.world.blocks.distribution.StackConveyor;
+
+import static mindustry.type.ItemStack.with;
 
 public class SBlocks implements ContentList {
     public static Block railWay;
@@ -15,11 +16,12 @@ public class SBlocks implements ContentList {
     public void load() {
         railWay = new StackConveyor("railWay"){{
 
+            health = 100;
             size = 1;
-            hasItems = true;
-            itemCapacity = 40;
-            conveyorPlacement = true;
-            requirements(Category.distribution, ItemStack.with(Items.copper, 10));
+            speed = 12f / 180f;
+            itemCapacity = 30;
+            conveyorPlacement = false;
+            requirements(Category.distribution, with(Items.copper, 10));
 
         }};
     }
