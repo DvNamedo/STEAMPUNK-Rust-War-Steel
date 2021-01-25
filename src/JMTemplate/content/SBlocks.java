52dda809b999880f3432a3537bca65c3b5f3b4e5
eukkbi <eukkbi6@gmail.com>
2021-01-25 23:58:20 +0900

    1/25

--- src/JMTemplate/content/SBlocks.java
+++ src/JMTemplate/content/SBlocks.java
@@ -1,5 +1,6 @@
 package JMTemplate.content;
 
+import mindustry.annotations.Annotations;
 import mindustry.content.Items;
 import mindustry.ctype.ContentList;
 import mindustry.type.Category;
@@ -24,10 +25,16 @@ public class SBlocks implements ContentList {
             requirements(Category.distribution, with(Items.copper, 10));
         }};
 
-        woodenBlocka = new Floor("woodenBlocka");
+        woodenBlocka = new Floor("woodenBlocka"){{
+            variants = 0;
+        }};
 
-        woodenBlockb = new Floor("woodenBlockb");
+        woodenBlockb = new Floor("woodenBlockb"){{
+            variants = 0;
+        }};
 
-        pollutedGrass = new Floor("pollutedGrass");
+        pollutedGrass = new Floor("pollutedGrass"){{
+            variants = 0;
+        }};
     }
 }
